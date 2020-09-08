5973c3b8e209438ede2ed8f6603480ace9d4a52e
wangao <1366463855@qq.com>
2020-09-08 09:29:49 +0800

    feat: init

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,16 @@ from flask import Flask,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_migrate import Migrate
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
 
-def create_app(config_name):
+def create_app():
     app = Flask(__name__)
+
+    config_name= os.getenv("FLASK_ENV" or "development")
+
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
