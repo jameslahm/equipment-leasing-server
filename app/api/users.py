a4cbf72ae45bbd889a1d2cc8398e583d30d87388
zhangzhi <z-zhang17@mails.tsinghua.edu.cn>
2020-09-09 20:47:30 +0800

    feat: finish user api

--- app/api/users.py
+++ app/api/users.py
@@ -2,6 +2,7 @@ from flask import Blueprint,Response,render_template,jsonify,request
 from flask import request,abort,make_response
 from flask import json, current_app
 from flask.helpers import flash, url_for
+from sqlalchemy.sql.expression import null
 from flask_mail import Mail, Message
 from ..models import User,Role
 from . import api
@@ -17,7 +18,6 @@ def send_async_email(app,msg):
 def send_mail(to,subject,template,token):
     app = current_app._get_current_object()
     msg = Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX']+subject,sender=current_app.config['MAIL_USERNAME'],recipients=[to])
-    print(template)
     msg.html = "<h3>请确认注册</h3><a href='http://127.0.0.1/confirm?confirm_token={token}'>http://127.0.0.1/confirm</a>"
     thr = Thread(target=send_async_email,args=[app,msg])
     thr.start()
@@ -40,7 +40,6 @@ def login():
 
 @api.route('/register',methods=['POST'])
 def register():
-    print('enter')
     data=request.json
     if data is None:
         return jsonify({"error":"bad request"}),400
@@ -54,9 +53,6 @@ def register():
         user = User.query.filter_by(username=username).first()
         if user is not None:
             return jsonify({'error':'this username has been registered'}),400
-        print(username)
-        print(email)
-        print(password)
         user = User(email = email,username = username,password = password)
         print('exit')
         jwt = user.generate_auth_token(expiration=86400*365)
@@ -65,7 +61,41 @@ def register():
         db.session.add(user)
         db.session.commit()
         send_mail(email,'确认你的账户','/confirm',jwt)
-        print(User_uncomfirmed)
         return jsonify(User_uncomfirmed)
 
+@api.route('/users',methods=['GET'])
+def get_users():
+    body=dict()
+    Users=dict()
+    if User.verify_auth_token(request.headers.get('Authorization')):
+        if request.args.get('username'):
+            body['username'] = request.args.get('username')
+        if request.args.get('page'):
+            body['page'] = request.args.get('page')
+        if request.args.get('page_size'):
+            body['page_size'] = request.args.get('page_size')
+        users,total = User.search_byusername(body)
+        users = [x.to_json() for x in users]
+        Users['total'] = total
+        Users['users'] = users
+        return jsonify(Users),200
+    return jsonify({'error':'invalid token'}),401
 
+@api.route('/users/<int:id>',methods=['GET','PUT','DELETE'])
+def operate_user(id):
+    operator = User.verify_auth_token(request.headers.get("Authorization"))
+    if not operator:
+        return jsonify({'error':'invalid token'}),401
+    if request.method == 'GET':
+        return jsonify(User.query.filter_by(id=id).first().to_json()),200
+    if request.method == 'PUT':
+        data = request.json
+        user = User.update_userinfo(id, operator, data)
+        if user is not null:
+            return jsonify(user.to_json()),200
+        return jsonify({'error':'illegal params'}),400
+    if request.method == 'DELETE':
+        user = User.delete_user(id,operator)
+        if user is not null:
+            return jsonify(user),200
+        return jsonify({'error':'no permission'}),401
