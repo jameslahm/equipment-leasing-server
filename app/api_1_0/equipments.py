bd3f32a901d9dbea92c2be453a81028f05c3f5b7
ZGPOWER <51812221+ZGPOWER@users.noreply.github.com>
2020-09-09 13:43:46 +0800

    Update equipments.py

--- app/api_1_0/equipments.py
+++ app/api_1_0/equipments.py
@@ -1,24 +1,21 @@
 from crypt import methods
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 from flask.globals import request
-from flask.helpers import make_response
-from .. import db
 from ..models import Equipment, User
-
-from .__init__ import api
+from . import api
 
 @api.route('/equipments',methods=['GET'])
 def get_all():
     current_user = User.verify_auth_token(request.headers.get("Authorization"))
     if not current_user:
-        return jsonify({"error":"error message"}), 401
+        return Response(jsonify({"error":"error message"}), 401)
     page = request.args.get("page", 1)
     page_size = request.args.get("page_size", 10)
     elist = Equipment.search_byusername(current_user, request.args)
     anslist = []
     for i in range((page-1)*page_size, min(len(elist),page*page_size)):
         anslist.append(elist[i])
-    return jsonify({"equipments":anslist,"total":len(anslist)})
+    return Response(jsonify({"equipments":anslist,"total":len(anslist)}))
     
 
 
@@ -26,13 +23,11 @@ def get_all():
 def equipment_operate(id):
     current_user = User.verify_auth_token(request.headers.get("Authorization"))
     if not current_user:
-        return jsonify({"error":"error message"}), 401
+        return Response(jsonify({"error":"error message"}), 401)
     if request.method == "GET":
-        return jsonify(Equipment.query.get(id).to_json())
+        return Response(jsonify(Equipment.query.get(id).to_json()))
     if request.method == "PUT":
         Equipment.update_equipment(id, current_user, request.form)
-        return jsonify(Equipment.query.get(id).to_json())
+        return Response(jsonify(Equipment.query.get(id).to_json()))
     if request.method == "DELETE":
-        return jsonify(Equipment.delete_equipment(id, current_user))
-
-
+        return Response(jsonify(Equipment.delete_equipment(id, current_user)))
