56c4b3ba99b5c50e68141acfcb69efd3b9cd633a
ZGPOWER <51812221+ZGPOWER@users.noreply.github.com>
2020-09-09 12:51:48 +0800

    Update equipments.py

--- app/api_1_0/equipments.py
+++ app/api_1_0/equipments.py
@@ -1,8 +1,6 @@
 from crypt import methods
-from flask import Flask
+from flask import Flask, jsonify
 from flask.globals import request
-import json
-
 from flask.helpers import make_response
 from .. import db
 from ..models import Equipment, User
@@ -13,14 +11,14 @@ from .__init__ import api
 def get_all():
     current_user = User.verify_auth_token(request.headers.get("Authorization"))
     if not current_user:
-        return make_response(json.dumps({"error":"error message"}), 401)
+        return jsonify({"error":"error message"})
     page = request.args.get("page", 1)
     page_size = request.args.get("page_size", 10)
     elist = Equipment.search_byusername(current_user, request.args)
     anslist = []
     for i in range((page-1)*page_size, min(len(elist),page*page_size)):
         anslist.append(elist[i])
-    return {"equipments":anslist,"total":len(anslist)}
+    return jsonify({"equipments":anslist,"total":len(anslist)})
     
 
 
@@ -28,13 +26,13 @@ def get_all():
 def equipment_operate(id):
     current_user = User.verify_auth_token(request.headers.get("Authorization"))
     if not current_user:
-        return make_response(json.dumps({"error":"error message"}), 401)
+        return jsonify({"error":"error message"})
     if request.method == "GET":
-        return Equipment.query.get(id).to_json()
+        return jsonify(Equipment.query.get(id).to_json())
     if request.method == "PUT":
         Equipment.update_equipment(id, current_user, request.form)
-        return Equipment.query.get(id).to_json()
+        return jsonify(Equipment.query.get(id).to_json())
     if request.method == "DELETE":
-        return Equipment.delete_equipment(id, current_user)
+        return jsonify(Equipment.delete_equipment(id, current_user))
 
 
