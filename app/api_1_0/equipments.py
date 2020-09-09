c7c902cec9af9e5dafc023e7134da46989203a33
ZGPOWER <51812221+ZGPOWER@users.noreply.github.com>
2020-09-09 13:57:21 +0800

    Update equipments.py

--- app/api_1_0/equipments.py
+++ app/api_1_0/equipments.py
@@ -1,6 +1,8 @@
 from crypt import methods
 from flask import Flask, jsonify, Response
+from flask import json
 from flask.globals import request
+from sqlalchemy.sql.expression import null
 from ..models import Equipment, User
 from . import api
 
@@ -8,7 +10,7 @@ from . import api
 def get_all():
     current_user = User.verify_auth_token(request.headers.get("Authorization"))
     if not current_user:
-        return Response(jsonify({"error":"error message"}), 401)
+        return Response(jsonify({"error":"unauthorized"}), 401)
     page = request.args.get("page", 1)
     page_size = request.args.get("page_size", 10)
     elist = Equipment.search_byusername(current_user, request.args)
@@ -23,11 +25,19 @@ def get_all():
 def equipment_operate(id):
     current_user = User.verify_auth_token(request.headers.get("Authorization"))
     if not current_user:
-        return Response(jsonify({"error":"error message"}), 401)
+        return Response(jsonify({"error":"unauthorized"}), 401)
     if request.method == "GET":
-        return Response(jsonify(Equipment.query.get(id).to_json()))
+        equipment = Equipment.query.filter_by(id=id).first()
+        if equipment is None:
+            return Response(jsonify({}))
+        return Response(jsonify(equipment.to_json()))
     if request.method == "PUT":
-        Equipment.update_equipment(id, current_user, request.form)
+        update = Equipment.update_equipment(id, current_user, request.form)
+        if update == null:
+            return Response(jsonify({"error":"no such equipment"}, 400))
         return Response(jsonify(Equipment.query.get(id).to_json()))
     if request.method == "DELETE":
-        return Response(jsonify(Equipment.delete_equipment(id, current_user)))
+        delete = Equipment.delete_equipment(id, current_user)
+        if delete == null:
+            return Response(jsonify({"error":"no such equipment"}), 400)
+        return Response(jsonify(delete.to_json()))
