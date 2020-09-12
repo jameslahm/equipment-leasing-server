fa52f04cadecbcc5cd1f990fca7892681fb0995b
wangao <1366463855@qq.com>
2020-09-13 01:58:01 +0800

    chore: reset

--- migrations/versions/3ff42b27ac95_.py
+++ migrations/versions/3ff42b27ac95_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 8968e424bffc
+Revision ID: 3ff42b27ac95
 Revises: 
-Create Date: 2020-09-12 23:41:03.209843
+Create Date: 2020-09-13 01:57:43.948244
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '8968e424bffc'
+revision = '3ff42b27ac95'
 down_revision = None
 branch_labels = None
 depends_on = None
